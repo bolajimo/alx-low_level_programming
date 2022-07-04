056eea13e6ab929c1b498a6745baeb359569ff88
bolajimo <bolajimoses17@gmail.com>
2022-07-04 16:50:33 -0700

    p

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,1 +1,24 @@
-gcc: error: 64: No such file or directory
+#include <stdio.h>
+
+/*
+ *  * Main - Prints out all the sizes of each variable 
+ *   
+ *    * Return: Always 0 (success)
+ *     */
+
+int main(void) {
+	char c;
+	int i;
+	long int l;
+	long long int L;
+	float r;
+
+	printf("Size of a char: %lu byte(s)\n",sizeof(c));
+	printf("Size of a int: %lu byte(s)\n",sizeof(i));
+	printf("Size of a long int: %lu byte(s)\n",sizeof(l));
+	printf("Size of a long long int: %lu byte(s)\n",sizeof(L));
+	printf("Size of a float: %lu byte(s)\n",sizeof(r));
+							
+	return (0);
+}
+
