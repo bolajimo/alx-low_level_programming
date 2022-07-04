2f502714707568374e424051f5f55954a996aaf1
bolajimo <bolajimoses17@gmail.com>
2022-07-04 13:02:07 -0700

    commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,3 +1,20 @@
-/usr/bin/ld: /usr/lib/gcc/x86_64-linux-gnu/9/../../../x86_64-linux-gnu/Scrt1.o: in function `_start':
-(.text+0x24): undefined reference to `main'
-collect2: error: ld returned 1 exit status
+#include <stdio.h>
+
+/**
+ *  * main - print single  digit numbers  of base 10 starting from 0
+ *   * followed by a new line
+ *    * Return: 0 if ( successful)
+ *    */
+
+int main(void)
+{
+	 int n;
+
+	  for (n = 48; n < 58; n++)
+		   {
+			     putchar(n);
+			      }
+	   putchar('\n');
+	    return (0);
+}
+
