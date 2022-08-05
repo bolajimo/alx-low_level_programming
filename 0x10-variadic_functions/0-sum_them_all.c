0917e0ce9b19a94d52dbfdec1c3e68190bbba369
bolajimo <bolajimoses17@gmail.com>
2022-08-05 23:57:54 +0100

    'commit'

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,14 +1,30 @@
-#include "function_pointers.h"
+/*
+ * File: 0-sum_them_all.c
+ * Auth: Tolulope Fakunle
+ */
+
+#include "variadic_functions.h"
+#include <stdarg.h>
 
 /**
- * print_name - prints a name.
- * @name: input name.
- * @f: function pointer.
+ * sum_them_all - Returns the sum of all its paramters.
+ * @n: The number of paramters passed to the function.
+ * @...: A variable number of paramters to calculate the sum of.
  *
- * Return: no return.
+ * Return: If n == 0 - 0.
+ *         Otherwise - the sum of all parameters.
  */
-void print_name(char *name, void (*f)(char *))
+int sum_them_all(const unsigned int n, ...)
 {
-	if (name && f)
-		f(name);
+	va_list nums;
+	unsigned int index, sum = 0;
+
+	va_start(nums, n);
+
+	for (index = 0; index < n; index++)
+		sum += va_arg(nums, int);
+
+	va_end(nums);
+
+	return (sum);
 }
